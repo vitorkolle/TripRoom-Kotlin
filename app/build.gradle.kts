fb0a3471b14fc75c374d18252c7a6b8c693a7a61
vitorkolle <vitorpaeskolle@gmail.com>
2024-05-07 17:25:02 -0300

    banco de dados em andamento

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -51,7 +52,6 @@ android {
 
 dependencies {
 
-    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -67,5 +67,10 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.compose.material:material-icons-extended-android:1.6.3")
+
+    // Dependências do Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
---
+++ app/src/main/java/br/senai/sp/jandira/triproom/model/Usuario.kt
@@ -0,0 +1,13 @@
+package br.senai.sp.jandira.triproom.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tbl_usuarios")
+data class Contato(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nome: String = "",
+    val email: String = "",
+    val senha: String = "",
+    val telefone: String = "",
+)
